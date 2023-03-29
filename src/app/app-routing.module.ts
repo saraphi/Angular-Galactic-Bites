296a1f149835bd8544dc61e55a9598c85751684b
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-03-29 15:44:29 +0100

    Created login-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginFormComponent } from './components/login-form/login-form.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  	selector: 'app-login-form',
+  	templateUrl: './login-form.component.html',
+  	styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent {
+	constructor(private router: Router, private fb: FormBuilder) {}
 
+	loginForm: FormGroup = this.fb.group({
+		password: ['', [Validators.required]],
+		email: ['', [Validators.required, Validators.email]],
+	});
+
+	get email() {
+		return this.loginForm.get('email');
+	}
+
+	get password() {
+		return this.loginForm.get('password');
+	}
+
+	signUp() { this.router.navigate(['/signup']); }
+	onSubmit() { this.router.navigate(['/profile']); }
 }
