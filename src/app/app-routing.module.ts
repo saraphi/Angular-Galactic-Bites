de86bb7356d00c7ef45b98498b3b039ac012f5c0
Albeg1 <agonzalezreyes369@gmail.com>
2023-04-10 12:25:58 +0100

    Added BannerComponent to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { BannerComponent} from "./components/banner/banner.component";
 
 const routes: Routes = [
-  { path: 'login', component: LoginFormComponent}
+  { path: 'login', component: LoginFormComponent},
+  { path: 'banner', component: BannerComponent}
 ];
 
 @NgModule({
