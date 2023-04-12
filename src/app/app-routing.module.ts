3044bf2c75d08907ac3b44570ff2581429690665
Albeg1 <agonzalezreyes369@gmail.com>
2023-04-12 09:20:53 +0100

    Added placeholder image for banner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
 import { PaymentFormComponent } from './components/payment-form/payment-form.component';
+import { BannerComponent} from "./components/banner/banner.component";
 
 const routes: Routes = [
   { path: 'login', component: LoginFormComponent },
   { path: 'signup', component: SignupFormComponent },
   { path: 'payment', component: PaymentFormComponent },
+  { path: 'banner', component: BannerComponent}
 ];
 
 @NgModule({
