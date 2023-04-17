3beeb97fecb4100b8d386db36255c4386748037e
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-04-17 12:25:12 +0100

    Updated edit profile popup

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/user.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
     selector: 'app-profile',
@@ -7,11 +10,34 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
-    triggerEditPopup() {
-        
+    /*@Input() user!: User;*/
+    showOverlay: boolean = false;
+    showPopup: boolean = false;
+
+    user: User = {
+        id: "0",
+        email: "saragonza.lez0608@gmail.com",
+        name: "Sara González Ramírez",
+        password_token: "Hola!9",
+        phone: "617 322 872",
+        points: 0
+    }
+
+    @ViewChild('overlay') overlay!: ElementRef<any>;
+
+    constructor(private route: ActivatedRoute, private userService: UserService) {}
+
+    togglePopup() {
+    }
+
+    toggleEditPopup() {
     }
 
     triggerDeletePopup() {
         
+    }
+
+    logout() {
+
     }
 }
