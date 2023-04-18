4bede1274574824835bf82333fac69972d043613
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-04-18 18:07:28 +0100

    deleted route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ import { MenuComponent } from './pages/menu/menu.component';
 import { OffersComponent } from './pages/offers/offers.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
-import { SliderProductComponent } from './components/slider-product/slider-product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -20,7 +19,6 @@ const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
   { path: 'shopping-cart', component: ShoppingCartComponent },
   { path: 'payment', component: PaymentFormComponent },
-  { path: 'slider-product', component: SliderProductComponent }
 ];
 
 @NgModule({
