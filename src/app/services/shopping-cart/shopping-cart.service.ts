194a13d92e4eaf79be072797e8808185388a9125
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-04 14:16:52 +0100

    fixed functionality of shopping cart

--- src/app/services/shopping-cart/shopping-cart.service.ts
+++ src/app/services/shopping-cart/shopping-cart.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { ShoppingCart } from 'src/app/models/shopping-cart';
 import { UserService } from '../user/user.service';
 import { ProductService } from '../product/product.service';
@@ -6,39 +6,27 @@ import { ProductService } from '../product/product.service';
 @Injectable({
   providedIn: 'root'
 })
-export class ShoppingCartService implements OnInit {
+export class ShoppingCartService {
 
-  shoppingCart: ShoppingCart | null = null;
+  shoppingCart: ShoppingCart = { items: {'0': 1, '1': 2}, totalPrice: 0 };
 
   constructor(private userService: UserService, private itemService: ProductService) {}
 
-  ngOnInit(): void {
-    if (this.userService.user) this.shoppingCart = this.getMyShoppingCart();
-  }
-
-  private getMyShoppingCart(): ShoppingCart | null {
-    return { items: {'0': 1, '1': 2}, totalPrice: 0 };
-  }
-
   getQuantity(itemId: string): number | null {
-    if (!this.shoppingCart) return null;
     return this.shoppingCart.items[itemId];
   }
 
   deleteItem(itemId: string): void {
-    if (!this.shoppingCart) return;
     delete this.shoppingCart.items[itemId];
   }
 
   addItem(itemId: string): void {
-    if (!this.shoppingCart) return;
     let quantity: number = 0;
     if (this.shoppingCart.items[itemId]) quantity = this.shoppingCart.items[itemId];
     this.shoppingCart.items[itemId] = quantity + 1;
   }
 
   removeItem(itemId: string): void {
-    if (!this.shoppingCart) return;
     let quantity: number = 0;
     if (this.shoppingCart.items[itemId]) quantity = this.shoppingCart.items[itemId];
     this.shoppingCart.items[itemId] = quantity - 1;
@@ -46,12 +34,9 @@ export class ShoppingCartService implements OnInit {
   }
 
   getTotalPrice(): number  {
-    if (!this.shoppingCart) return 0;
-
     this.shoppingCart.totalPrice = 0;
 
     Object.keys(this.shoppingCart.items).forEach((key: string) => {
-      if (this.shoppingCart)
       this.shoppingCart.totalPrice += this.itemService.getItemPrice(key) * this.shoppingCart.items[key];
     })
 
@@ -59,7 +44,6 @@ export class ShoppingCartService implements OnInit {
   }
 
   getItemsKeys(): string[] {
-    if (!this.shoppingCart) return [];
     return Object.keys(this.shoppingCart.items);
   } 
 }
