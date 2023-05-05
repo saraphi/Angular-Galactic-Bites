80efeebf6218ed68550b08eb79b706eedcc17f41
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-05 15:09:54 +0100

    fixed a bug in shopping cart

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -9,9 +9,9 @@ export class ProductService {
 
   // NOT FINAL
   products: {[key: string]: Product} = {
-    // '0': { id: '0', image: '../../../assets/placeholder.png', name: 'Pizza', description: 'Pizza rica', price: 10.8, discount: 0, category: Category.main },
-    // '1': { id: '1', image: '../../../assets/placeholder.png', name: 'Borguesa', description: 'Borguesa wena', price: 6.2, discount: 0, category: Category.main },
-    // '2': { id: '2', image: '../../../assets/placeholder.png', name: 'Taquito', description: 'Taquito sabroso', price: 2.5, discount: 0, category: Category.main }
+    '0': { id: '0', image: '../../../assets/placeholder.png', name: 'Pizza', description: 'Pizza rica', price: 10.8, discount: 0, category: Category.main },
+    '1': { id: '1', image: '../../../assets/placeholder.png', name: 'Borguesa', description: 'Borguesa wena', price: 6.2, discount: 0, category: Category.main },
+    '2': { id: '2', image: '../../../assets/placeholder.png', name: 'Taquito', description: 'Taquito sabroso', price: 2.5, discount: 0, category: Category.main }
   }
 
   getProducts(): Product[] {
