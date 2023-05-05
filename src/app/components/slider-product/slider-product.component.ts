c439d87de06e8f93adff836dc396b2dfad377015
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-05 20:17:11 +0100

    made basic slider functionality

--- src/app/components/slider-product/slider-product.component.ts
+++ src/app/components/slider-product/slider-product.component.ts
@@ -3,6 +3,7 @@ import { Product } from 'src/app/models/product';
 import { ProductComponent } from '../product/product.component';
 import { ProductService } from 'src/app/services/product/product.service';
 import { Category } from 'src/app/models/category';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-slider-product',
@@ -11,12 +12,32 @@ import { Category } from 'src/app/models/category';
 })
 export class SliderProductComponent implements OnInit {
 
-  // products: Product[] = [];
-  categories: string[] = [Category.menu, Category.pizza, Category.burguer, Category.taco, Category.main, Category.complement, Category.drink]
+  categories: string[] = [];
+  carouselItems: string[] = [];
+  position: number = 0;
 
-  constructor(private productService: ProductService) {}
+  constructor() {
+    this.categories = [Category.menu, Category.pizza, Category.burguer, Category.taco, Category.main, Category.complement, Category.drink];
+    this.carouselItems = this.categories.slice(0, 3);
+  }
 
   ngOnInit(): void {
-      // this.products = this.productService.getProducts();
+    this.updateCarouselItems();
+  }
+
+  updateCarouselItems() {
+    this.carouselItems = this.categories.slice(this.position, this.position+3);
+  }
+
+  moveLeft(): void {
+    if (this.position == 0) return;
+    this.position -= 1; 
+    this.updateCarouselItems();
+  }
+
+  moveRight(): void {
+    if (this.position == this.categories.length - 3) return;
+    this.position += 1;
+    this.updateCarouselItems();
   }
 }
