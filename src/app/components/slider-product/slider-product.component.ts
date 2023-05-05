d0a2823497feae8bb7e01e7e4fe8d820e039a43a
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-05 12:11:56 +0100

    commented firebase not final implementation

--- src/app/components/slider-product/slider-product.component.ts
+++ src/app/components/slider-product/slider-product.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
-import { Product } from 'src/app/models/product';
-import { ProductComponent } from '../product/product.component';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 import { FirebaseDataService } from 'src/app/services/database/firebase-data.service';
+import { ProductService } from 'src/app/services/product/product.service';
+
 @Component({
   selector: 'app-slider-product',
   templateUrl: './slider-product.component.html',
   styleUrls: ['./slider-product.component.scss']
 })
-export class SliderProductComponent {
+export class SliderProductComponent implements OnInit {
 
-  products: ProductComponent[]
+  products: Product[] = [];
 
-  constructor() {
-    this.products = [];
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+      this.products = this.productService.getProducts();
   }
 }
