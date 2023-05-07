3df5fbf97dc9a4ac77e48c0b5beb53c2f20837b6
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-07 10:54:38 +0100

    menu showing and scrolling works

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,15 +1,23 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-// import { AuthService } from 'src/app/services/database/firebase-auth.service';
+import { Category } from 'src/app/models/category';
+import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit{
-  // constructor(private authservices: AuthService) { }
-  
-  ngOnInit() {
-    //this.authservices.signUp();
+export class MenuComponent {
+  categories: string[] = [Category.main, Category.complement, Category.drink, Category.dessert];
+
+  constructor(private productService: ProductService, private viewportScroller: ViewportScroller) {}
+
+  getProducts(category: string): string[] {
+    return this.productService.getProdutsIdByCategory(category);
+  }
+
+  scroll(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
   }
 }
