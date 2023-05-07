9f5fb2c7497553901490ec9026fb22fe4d3e7229
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-07 11:28:03 +0100

    feature/profile

--- src/app/components/delete-profile-popup/delete-profile-popup.component.ts
+++ src/app/components/delete-profile-popup/delete-profile-popup.component.ts
@@ -1,12 +1,65 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Form } from 'src/app/models/form';
 import { User } from 'src/app/models/user';
+import { PasswordValidator } from 'src/app/validators/password.validator';
 
 @Component({
   selector: 'app-delete-profile-popup',
   templateUrl: './delete-profile-popup.component.html',
   styleUrls: ['./delete-profile-popup.component.scss']
 })
-export class DeleteProfilePopupComponent {
+export class DeleteProfilePopupComponent implements Form {
 
   @Input() user!: User;
+  @Output() close: EventEmitter<void> = new EventEmitter<void>();
+
+  deleteForm: FormGroup;
+
+  @ViewChild('input') input!: ElementRef;
+
+  constructor(private fb: FormBuilder) {
+    this.deleteForm = this.fb.group({
+      password: ['', [Validators.required, PasswordValidator.strong()]]
+    })
+  }
+
+  onError(input: ElementRef): void {
+		input.nativeElement.style.boxShadow = '0px 0px 10px rgb(255, 70, 92)';
+	}
+	
+	checkErrors(): boolean {
+		let errors: boolean = false;
+
+    const control = this.deleteForm.controls['password'];
+
+    if (control.errors) {
+      console.log('errors:', control.errors)
+      this.onError(this.input);
+      errors = true;
+    }
+
+		return errors;
+	}
+
+	resetErrors(): void {
+		this.input.nativeElement.style.boxShadow = 'none';
+	}
+
+  onClose(): void {
+    this.close.emit();
+  }
+
+  onSubmit(): void {
+    this.resetErrors();
+    if (this.checkErrors()) return;
+
+    console.log('checking password...');
+    // checkPassword, que devuelva un booleano, y con el resultado del booleano,
+    // si es false:
+    // this.onError(this.input);
+    
+    console.log('deleting...');
+    this.onClose();
+  }
 }
